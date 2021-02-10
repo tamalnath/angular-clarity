9d1b0456f04a4e2cc9188d8a0618e1997a59becd
Tamal Nath <tamal.nath@gmail.com>
2021-02-10 16:35:16 +0530

    Added routes and clarity

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CoreComponent } from './core/core.component';
+import { AngularComponent } from './angular/angular.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'core', component: CoreComponent },
+  { path: 'angular', component: AngularComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
