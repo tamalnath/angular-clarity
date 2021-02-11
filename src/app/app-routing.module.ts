df48e88510af0631443bdc2c2311c8d3ccb0c00d
Tamal Nath <tamal.nath@gmail.com>
2021-02-11 10:27:41 +0530

    Added child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,29 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CoreComponent } from './core/core.component';
 import { AngularComponent } from './angular/angular.component';
+import { WizardComponent } from './angular/wizard/wizard.component';
+import { ModalComponent } from './angular/modal/modal.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
   { path: 'core', component: CoreComponent },
-  { path: 'angular', component: AngularComponent },
+  { path: 'angular', component: AngularComponent, children: [
+    { path: 'modal', component: ModalComponent },
+    { path: 'wizard', component: WizardComponent },
+  ] },
+  { path: '', component: HomeComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
+  declarations: [
+    AngularComponent,
+    CoreComponent,
+    HomeComponent,
+    ModalComponent,
+    NotFoundComponent,
+    WizardComponent,
+  ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
