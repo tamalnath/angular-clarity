742229783ba5f5415c7cd23f0a6dd828bac9d722
Tamal Nath <tamal.nath@gmail.com>
2021-02-13 09:42:57 +0530

    Added form to Wizard

--- src/app/angular/wizard/wizard.component.ts
+++ src/app/angular/wizard/wizard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CITY_STATES } from '../../utility.service';
 
 @Component({
   selector: 'app-wizard',
@@ -7,12 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WizardComponent implements OnInit {
 
+  cityStates = CITY_STATES;
   open: boolean = false;
+
+  name: string = '';
+  dateOfBirth: string = '';
+  address: string = '';
+  city: string = '';
+  state: string = '';
+  permanent: boolean = false;
+  form = { name: "", dateOfBirth: "", address: "", city: "", permanent: false };
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  save(): void {
+    this.name = this.form.name;
+    this.address = this.form.address;
+    this.city = this.form.city;
+    this.state = this.cityStates[this.city];
+    this.permanent = this.form.permanent;
+    this.dateOfBirth = this.form.dateOfBirth;
+    this.open = false;
+  }
   
 }
